5daff9bbc6c78322cd65ec7a9ae4a19135eb37e8
Ajay Singh <ajaysij@amazon.com>
2024-07-01 02:19:14 +0530

    added TPC server code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io::{Write};
+use std::net::{TcpListener};
+
 fn main() {
-    println!("Hello, world!");
+    println!("******************! Flash Cache Server !*******************");
+
+    let listener: TcpListener = TcpListener::bind("127.0.0.1:6379").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => {
+                println!("accepted new connection");
+                let response = "+PONG\r\n";
+                stream.write(response.as_bytes()).unwrap();
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
+    }
 }
